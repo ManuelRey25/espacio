fcb3a6921da78b15e552f3e8ff99d470f11a7045
Manuel Rey <122846332+ManuelRey25@users.noreply.github.com>
2023-09-17 17:29:28 -0300

    Add files via upload

---
+++ nosotros/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+function toggleSkills() {
+    var skills = document.getElementsByClassName("cover-letter");
+    for (var i = 0; i < skills.length; i++) {
+      if (skills[i].style.display === "none") {
+        skills[i].style.display = "block";
+      } else {
+        skills[i].style.display = "none";
+      }
+    }
+  }
+  
